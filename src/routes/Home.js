1f4a7a9b9f19b75ff304a17d0a487d5fea61aa8b
joohan <ddoachi@gmail.com>
2025-02-09 23:20:08 +0900

    Added `container` class to `Home`

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,6 +4,8 @@ import Headline from '../components/Headline'
 export default class Home extends Component {
   render() {
     const headline = new Headline().el
+
+    this.el.classList.add('container')
     this.el.append(
       headline
     )
