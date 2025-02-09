316260d83dbb41e28b8763a5639e7752c9a9a0e8
joohan <ddoachi@gmail.com>
2025-02-10 00:29:16 +0900

    Added to Home

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,5 +3,11 @@ import { Component } from '../core/common'
 export default class Search extends Component {
   render() {
     this.el.classList.add('search');
+    this.el.innerHTML = /* html */ `
+      <input placeholder="Enter a movie title" />
+      <button class"btn btn-primary">
+        Search!
+      </button>
+    `
   }
 }
