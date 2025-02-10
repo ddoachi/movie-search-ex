74e44148cba099acc77aa05558a5da30e0ba88a6
joohan <ddoachi@gmail.com>
2025-02-10 23:09:40 +0900

    Added Error Handling when no search results

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -11,16 +11,21 @@ export default class MovieList extends Component {
     movieStore.subscribe('loading', () => {
       this.render();
     })
+    movieStore.subscribe('message', () => {
+      this.render();
+    })
   }
   render() {
     this.el.classList.add('movie-list')
     this.el.innerHTML = /* html */ `
-      <div class="movies"></div>
+      ${movieStore.state.message
+      ? `<div class="message">${movieStore.state.message}</div>`
+      : '<div class="movies"></div>'}
       <div class="the-loader hide"></div>
     `
 
     const movieEl = this.el.querySelector('.movies');
-    movieEl.append(
+    movieEl?.append(
       ...movieStore.state.movies.map(
         movie => new MovieItem({ movie }).el
       )
--- src/store/movie.js
+++ src/store/movie.js
@@ -5,7 +5,8 @@ const store = new Store({
   page: 1,
   pageMax: 1,
   movies: [],
-  loading: false
+  loading: false,
+  message: 'Search for the movie title!'
 })
 
 export default store;
@@ -14,13 +15,18 @@ export const searchMovies = async page => {
   store.state.page = page;
   if ( page === 1 ) {
     store.state.movies = [];
+    store.state.message = '';
   }
   const res = await fetch(`https://www.omdbapi.com?apikey=f25e888&s=${store.state.searchText}&page=${page}`);
-  const { Search, totalResults } = await res.json();
-  store.state.movies = [
-    ...store.state.movies,
-    ...Search
-  ]
-  store.state.pageMax = Math.ceil(Number(totalResults) / 10);
+  const { Search, totalResults, Response, Error } = await res.json();
+  if ( Response === "True") {
+    store.state.movies = [
+      ...store.state.movies,
+      ...Search
+    ]
+    store.state.pageMax = Math.ceil(Number(totalResults) / 10);
+  } else {
+    store.state.message = Error;
+  }
   store.state.loading = false;
 }
