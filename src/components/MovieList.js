01f576f70a559f18b1ab7fb7505d429d34e94ee3
joohan <ddoachi@gmail.com>
2025-02-10 21:37:09 +0900

    Can print movie info without CSS style

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,5 +1,6 @@
 import { Component } from '../core/common'
 import movieStore from '../store/movie'
+import MovieItem from './MovieItem'
 
 export default class MovieList extends Component {
   constructor() {
@@ -16,9 +17,9 @@ export default class MovieList extends Component {
 
     const movieEl = this.el.querySelector('.movies');
     movieEl.append(
-      movieStore.state.movies.map(movie => {
-        return movie.Title
-      })
+      ...movieStore.state.movies.map(
+        movie => new MovieItem({ movie }).el
+      )
     )
 
   }
