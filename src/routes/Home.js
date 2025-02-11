0d20ac9eea53bdffc8a99e2e60f6855be096504b
joohan <ddoachi@gmail.com>
2025-02-11 22:44:24 +0900

    Begin implementing `TheHeader`

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,6 +3,7 @@ import Headline from '../components/Headline'
 import Search from '../components/Search'
 import MovieList from '../components/MovieList'
 import MovieListMore from '../components/MovieListMore'
+import TheHeader from '../components/TheHeader'
 
 export default class Home extends Component {
   render() {
@@ -10,9 +11,11 @@ export default class Home extends Component {
     const search = new Search().el
     const movieList = new MovieList().el
     const movieListMore = new MovieListMore().el
+    const header = new TheHeader().el
 
     this.el.classList.add('container')
     this.el.append(
+      header,
       headline,
       search,
       movieList,
