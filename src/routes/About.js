cfe6696ccf6ca8b0ec10058cfcda11f85152e5a4
joohan <ddoachi@gmail.com>
2025-02-11 23:49:37 +0900

    print some info in `About`

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,9 +1,23 @@
 import { Component } from '../core/common'
+import aboutStore from '../store/about'
 
 export default class About extends Component {
   render() {
+    const { photo, name, email, github, blog } = aboutStore.state
     this.el.classList.add('container', 'about')
     this.el.innerHTML = /* HTML */ `
+      <div
+        style="background-image: url(${photo})"
+        class="photo"></div>
+      <p class="name">${name}</p>
+      <p>
+        <a href="https://mail.google.com/mail/?view=cm&fs=1&to=${email}"
+           target="_blank">
+          ${email}
+        </a>
+      </p>
+      <p><a href="${github}" target="_blank"></a></p>
+      <p><a href="${blog}" target="_blank"></a></p>
     `
   }
   
