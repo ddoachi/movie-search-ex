b5ac959d74121f0f5cbf1f38eebefc185f7138e3
joohan <ddoachi@gmail.com>
2025-02-13 23:18:49 +0900

    Moved fetch part to `/api/movie.js`

---
+++ api/movie.js
@@ -0,0 +1,10 @@
+export default async function handler(request, response) {
+  const { title, page, id } = JSON.parse(request.body);
+  const url = id
+  ?  `https://www.omdbapi.com?apikey=f25e888&i=${id}&plot=full`
+  : `https://www.omdbapi.com?apikey=f25e888&s=${title}&page=${page}`
+
+  const res = await fetch(url);
+  const json = await res.json();
+  response.status(200).json(json)
+}
