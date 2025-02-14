f5f5b64fec18d3d0e55d37de9a32a5a536f8ebb1
joohan <ddoachi@gmail.com>
2025-02-14 21:51:34 +0900

    .js -> .ts for router function

--- src/core/common.ts
+++ src/core/common.ts
@@ -31,32 +31,46 @@ export class Component {
   }
 }
 
+interface Route {
+  path: string
+  component: typeof Component
+}
+type Routes = Route[]
+
 //// Router ////
-function routeRender(routes) {
+function routeRender(routes: Routes) {
   if (!location.hash) {
     history.replaceState(null, '', '#/');
   }
   const routerView = document.querySelector('router-view');
   const [hash, queryString = ''] = location.hash.split('?');
 
+  interface Query {
+    [key: string]: string
+  }
+
   // a=123&b=456
   const query = queryString
     .split('&')
-    .reduce((acc, cur) => {
+    .reduce((acc: Query, cur: string) => {
       const [key, value] = cur.split('=')
       acc[key] = value
       return acc
-    }, {})
+    }, {} as Query)
     history.replaceState(query, '');
 
-  const currentRoute = routes.find(route => new RegExp(`${route.path}/?$`).test(hash));
-  routerView.innerHTML = '';
-  routerView.append(new currentRoute.component().el)
+  const currentRoute = routes
+    .find(route => new RegExp(`${route.path}/?$`).test(hash));
+
+  if (routerView) {
+    routerView.innerHTML = '';
+    currentRoute && routerView.append(new currentRoute.component().el)
+  }
 
   window.scrollTo(0, 0);
 }
 
-export function createRouter(routes) {
+export function createRouter(routes: Routes) {
   return function () {
     window.addEventListener('popstate', () => {
       routeRender(routes);
